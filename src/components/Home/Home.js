1d715414c86868837cf7fa89c3e32566ca9060dd
Hafizur <hafizurrahmanbu@gmail.com>
2022-07-02 01:47:56 +0600

    added npm slide show package

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import './Home.css';
-import heroImg from '../../Images/hero-image.jpg';
+import heroImg1 from '../../Images/heroImages/heroimg1.jpg';
+import heroImg2 from '../../Images/heroImages/heroimg2.jpg'
+import heroImg3 from '../../Images/heroImages/heroimg3.jpg'
+import heroImg4 from '../../Images/heroImages/heroimg4.jpg'
+import heroImg5 from '../../Images/heroImages/heroimg5.jpg'
+import heroImg6 from '../../Images/heroImages/heroimg6.jpg'
+import heroImg7 from '../../Images/heroImages/heroimg7.jpg'
+import heroImg8 from '../../Images/heroImages/heroimg8.jpg'
+import heroImg9 from '../../Images/heroImages/heroimg9.jpg'
+import heroImg10 from '../../Images/heroImages/heroimg10.jpg'
+import heroImg11 from '../../Images/heroImages/heroimg11.jpg'
+import heroImg12 from '../../Images/heroImages/heroimg12.jpg'
 import { Link } from 'react-router-dom';
 import Typewriter from 'typewriter-effect';
 import GetId from '../../useState/GetId';
+
+import { Zoom } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
 
 const Home = () => {
     const [imageId, setImageId, control, setControl, darkMode, setDarkMode] = GetId();
@@ -63,25 +77,100 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
+
                 <div className="col-md-6">
                     <div className="hero-image-section">
                         <div className="hero-image-container">
-                            {/* <img className='hero-image' src={heroImg} alt="" /> */}
-
-                            <div className="row p-2">
+                            <div className="row">
                                 <div className="col-md-6 col-6">
-                                    <img className='hero-image' src={heroImg} alt="" />
+                                    <Zoom duration={1500} indicators={index => <div className="indicator">{index + 1}</div>} scale={1.4}>
+                                        <div className="each-slide-effect">
+                                            <div style={{
+                                                backgroundImage: `url(${heroImg1})`
+                                            }}>
+                                            </div>
+                                        </div>
+                                        <div className="each-slide-effect">
+                                            <div style={{
+                                                backgroundImage: `url(${heroImg2})`
+                                            }}>
+                                            </div>
+                                        </div>
+                                        <div className="each-slide-effect">
+                                            <div style={{
+                                                backgroundImage: `url(${heroImg3})`
+                                            }}>
+                                            </div>
+                                        </div>
+                                    </Zoom>
                                 </div>
                                 <div className="col-md-6 col-6">
-                                    <img className='hero-image' src={heroImg} alt="" />
+                                    <Zoom duration={1500} indicators={index => <div className="indicator">{index + 1}</div>} scale={1.4}>
+                                        <div className="each-slide-effect">
+                                            <div style={{
+                                                backgroundImage: `url(${heroImg4})`
+                                            }}>
+                                            </div>
+                                        </div>
+                                        <div className="each-slide-effect">
+                                            <div style={{
+                                                backgroundImage: `url(${heroImg5})`
+                                            }}>
+                                            </div>
+                                        </div>
+                                        <div className="each-slide-effect">
+                                            <div style={{
+                                                backgroundImage: `url(${heroImg6})`
+                                            }}>
+                                            </div>
+                                        </div>
+                                    </Zoom>
                                 </div>
                             </div>
-                            <div className='row p-2'>
+                            <div className="row">
                                 <div className="col-md-6 col-6">
-                                    <img className='hero-image' src={heroImg} alt="" />
+                                    <Zoom duration={1500} indicators={index => <div className="indicator">{index + 1}</div>} scale={1.4}>
+                                        <div className="each-slide-effect">
+                                            <div style={{
+                                                backgroundImage: `url(${heroImg7})`
+                                            }}>
+                                            </div>
+                                        </div>
+                                        <div className="each-slide-effect">
+                                            <div style={{
+                                                backgroundImage: `url(${heroImg8})`
+                                            }}>
+                                            </div>
+                                        </div>
+                                        <div className="each-slide-effect">
+                                            <div style={{
+                                                backgroundImage: `url(${heroImg9})`
+                                            }}>
+                                            </div>
+                                        </div>
+                                    </Zoom>
                                 </div>
                                 <div className="col-md-6 col-6">
-                                    <img className='hero-image' src={heroImg} alt="" />
+                                    <Zoom duration={1500} indicators={index => <div className="indicator">{index + 1}</div>} scale={1.4}>
+                                        <div className="each-slide-effect">
+                                            <div style={{
+                                                backgroundImage: `url(${heroImg10})`
+                                            }}>
+                                            </div>
+                                        </div>
+                                        <div className="each-slide-effect">
+                                            <div style={{
+                                                backgroundImage: `url(${heroImg11})`
+                                            }}>
+                                            </div>
+                                        </div>
+                                        <div className="each-slide-effect">
+                                            <div style={{
+                                                backgroundImage: `url(${heroImg12})`
+                                            }}>
+                                            </div>
+                                        </div>
+                                    </Zoom>
                                 </div>
                             </div>
                         </div>
@@ -101,7 +190,7 @@ const Home = () => {
                                         <h3 className='pt-2'>Add Title</h3>
                                     </div>
                                     <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad voluptatem fugit fuga beatae nemo ea officia, deleniti possimus repudiandae, aspernatur repellendus nisi expedita harum sunt fugiat tempore ab odit cupiditate.
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit.Ad voluptatem fugit fuga beatae nemo ea officia, deleniti possimus repudiandae, aspernatur repellendus nisi expedita harum sunt fugiat tempore ab odit cupiditate.
                                     </p>
                                 </div>
                             </div>
@@ -117,7 +206,7 @@ const Home = () => {
                                         <h3 className='pt-2'>Add Title</h3>
                                     </div>
                                     <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad voluptatem fugit fuga beatae nemo ea officia, deleniti possimus repudiandae, aspernatur repellendus nisi expedita harum sunt fugiat tempore ab odit cupiditate.
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit.Ad voluptatem fugit fuga beatae nemo ea officia, deleniti possimus repudiandae, aspernatur repellendus nisi expedita harum sunt fugiat tempore ab odit cupiditate.
                                     </p>
                                 </div>
                             </div>
@@ -133,7 +222,7 @@ const Home = () => {
                                         <h3 className='pt-2'>Add Title</h3>
                                     </div>
                                     <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad voluptatem fugit fuga beatae nemo ea officia, deleniti possimus repudiandae, aspernatur repellendus nisi expedita harum sunt fugiat tempore ab odit cupiditate.
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit.Ad voluptatem fugit fuga beatae nemo ea officia, deleniti possimus repudiandae, aspernatur repellendus nisi expedita harum sunt fugiat tempore ab odit cupiditate.
                                     </p>
                                 </div>
                             </div>
@@ -142,7 +231,7 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
